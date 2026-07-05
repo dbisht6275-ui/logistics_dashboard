31136365bf4bcf7c503a4baf38715e4b34c5afa1
Dinesh Bisht <dbisht6275@gmail.com>
2026-07-05 13:05:01 +0530

    Added deployment configuration files

---
+++ postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
