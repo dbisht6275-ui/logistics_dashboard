06264c576d37cfa8a62bc3e7a4e36270159d6f40
Dinesh Bisht <dbisht6275@gmail.com>
2026-07-11 19:40:39 +0530

    updated

--- pages/Home/overview_tab.py
+++ pages/Home/overview_tab.py
@@ -750,7 +750,7 @@ def show_overview():
         .reset_index()
     )
 
-    monthly_weight["Weight MT"] = (monthly_weight["aweight"] / 1000).round(0)
+    monthly_weight["Weight MT"] = (monthly_weight["aweight"] / 1000).round(1)
 
     monthly_weight["Month"] = pd.Categorical(
         monthly_weight["Month"],
